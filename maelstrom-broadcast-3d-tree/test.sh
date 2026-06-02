cd25b2712494fd6806a91ec9956d1a1ccb3254e8
teleivo <teleivo@users.noreply.github.com>
2026-06-02 10:13:53 +0200

    allow passing additional args to maelstrom

--- maelstrom-broadcast-3d-tree/test.sh
+++ maelstrom-broadcast-3d-tree/test.sh
@@ -4,4 +4,4 @@
 set -euo pipefail
 
 go build -o maelstrom-broadcast .
-maelstrom test -w broadcast --bin ./maelstrom-broadcast --node-count 25 --time-limit 20 --rate 100 --latency 100
+maelstrom test -w broadcast --bin ./maelstrom-broadcast --node-count 25 --time-limit 20 --rate 100 --latency 100 "$@"
