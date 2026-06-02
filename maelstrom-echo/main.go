f2257ff83843137717f79a0b92b29fa559934850
teleivo <teleivo@users.noreply.github.com>
2026-06-02 10:19:04 +0200

    add test scripts for challenge 1 and 2

--- maelstrom-echo/main.go
+++ maelstrom-echo/main.go
@@ -7,19 +7,18 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+// Challenge #1: Echo
+// https://fly.io/dist-sys/1/
 func main() {
 	n := maelstrom.NewNode()
 	n.Handle("echo", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		// Update the message type to return back.
 		body["type"] = "echo_ok"
 
-		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
 	})
 
