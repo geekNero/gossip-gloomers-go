ee68b8e96a172f6c003a5258c98483be97f6afb5
teleivo <teleivo@users.noreply.github.com>
2026-06-03 10:01:17 +0200

    wip

--- maelstrom-counter/main.go
+++ maelstrom-counter/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
+	"maps"
 	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -11,13 +13,19 @@ import (
 // Challenge #4: Grow-Only Counter
 // https://fly.io/dist-sys/4/
 
-var (
-	mu      sync.Mutex
-	counter int
-)
+// idea 1: store classic g-counter map of nodeID to increment under one key and then sum up on read
+// TODO am I doing this correctly? with the sequential kv do I need to merge?
+// idea 2: each node stores its int counter under its own key and on read reads every key from the
+// kv as each node knows all node ids
+
+// TODO on partition will I be able to talk to kv?
 
 func main() {
 	n := maelstrom.NewNode()
+	kv := maelstrom.NewSeqKV(n)
+
+	var counter map[string]int
+	var mu sync.RWMutex
 
 	n.Handle("add", func(msg maelstrom.Message) error {
 		var body struct {
@@ -27,7 +35,18 @@ func main() {
 			return err
 		}
 
-		// TODO: implement
+		newCounter := make(map[string]int, len(n.NodeIDs()))
+		mu.RLock()
+		maps.Copy(newCounter, counter)
+		newCounter[n.ID()] += body.Delta
+		err := kv.CompareAndSwap(context.Background(), "counter", counter, newCounter, true)
+		if err != nil {
+			// TODO what now; retry I guess
+			if maelstrom.ErrorCode(err) == maelstrom.PreconditionFailed {
+				// retry
+			}
+		}
+		mu.RUnlock()
 
 		return n.Reply(msg, map[string]any{
 			"type": "add_ok",
@@ -35,11 +54,20 @@ func main() {
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		// TODO: implement
+		result := make(map[string]int)
+		err := kv.ReadInto(context.Background(), "counter", &result)
+		if err != nil {
+			// TODO err or just return 0?
+		}
+
+		var sum int
+		for _, v := range result {
+			sum += v
+		}
 
 		return n.Reply(msg, map[string]any{
 			"type":  "read_ok",
-			"value": 0,
+			"value": sum,
 		})
 	})
 
--- maelstrom-counter/test.sh
+++ maelstrom-counter/test.sh
@@ -3,4 +3,6 @@
 set -euo pipefail
 
 go build -o maelstrom-counter .
-maelstrom test -w g-counter --bin ./maelstrom-counter --node-count 3 --rate 100 --time-limit 20 --nemesis partition "$@"
+# maelstrom test -w g-counter --bin ./maelstrom-counter --node-count 3 --rate 100 --time-limit 20 --nemesis partition "$@"
+# maelstrom test -w g-counter --bin ./maelstrom-counter --node-count 3 --rate 100 --time-limit 20 "$@"
+maelstrom test -w g-counter --bin ./maelstrom-counter --node-count 3 --rate 10 --time-limit 4 "$@"
